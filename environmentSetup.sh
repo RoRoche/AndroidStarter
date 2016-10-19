f6c66defbd2e272b76d05631e5c23440ef3a58f7
Romain Rochegude <RoRoche@users.noreply.github.com>
2016-10-19 16:19:59 +0200

    Add files via upload

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+﻿#!/bin/bash
 
 # Fix the CircleCI path
 function getAndroidSDK {
@@ -7,13 +7,13 @@ function getAndroidSDK {
   DEPS="$ANDROID_HOME/installed-dependencies"
 
   if [ ! -e $DEPS ]; then
-    echo y | android update sdk -u -a -t android-19 &&
+    echo y | android update sdk -u -a -t android-24 &&
     echo y | android update sdk -u -a -t platform-tools &&
-    echo y | android update sdk -u -a -t build-tools-21.1.2 &&
+    echo y | android update sdk -u -a -t build-tools-23.0.3 &&
     echo y | android update sdk -u -a -t "extra-android-m2repository" &&
     echo y | android update sdk -u -a -t "extra-android-support" &&
     echo y | android update sdk -u -a -t "extra-google-m2repository" &&
-    echo no | android create avd -n testAVD -f -t android-19 --abi default/armeabi-v7a &&
+    echo no | android create avd -n testAVD -f -t android-24 --abi default/armeabi-v7a &&
     touch $DEPS
   fi
 }
