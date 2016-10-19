8956ca370247ecba695b83cae9036b1504ff6395
Romain Rochegude <RoRoche@users.noreply.github.com>
2016-10-19 16:17:06 +0200

    circleCi config

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -7,23 +7,23 @@ function getAndroidSDK {
   DEPS="$ANDROID_HOME/installed-dependencies"
 
   if [ ! -e $DEPS ]; then
-    echo y | android update sdk -u -a -t android-24 &&
+    echo y | android update sdk -u -a -t android-19 &&
     echo y | android update sdk -u -a -t platform-tools &&
-    echo y | android update sdk -u -a -t build-tools-23.0.3 &&
-    echo y | android update sdk -u -a -t extra-android-m2repository &&
-    echo y | android update sdk -u -a -t extra-android-support &&
-    echo y | android update sdk -u -a -t extra-google-m2repository &&
-    echo no | android create avd -n testAVD -f -t android-24 --abi default/armeabi-v7a &&
+    echo y | android update sdk -u -a -t build-tools-21.1.2 &&
+    echo y | android update sdk -u -a -t "extra-android-m2repository" &&
+    echo y | android update sdk -u -a -t "extra-android-support" &&
+    echo y | android update sdk -u -a -t "extra-google-m2repository" &&
+    echo no | android create avd -n testAVD -f -t android-19 --abi default/armeabi-v7a &&
     touch $DEPS
   fi
 }
 
-function waitAVD {
-    local bootanim=""
-    export PATH=$(dirname $(dirname $(which android)))/platform-tools:$PATH
-    until [[ "$bootanim" =~ "stopped" ]]; do
-      sleep 5
-      bootanim=$(adb -e shell getprop init.svc.bootanim 2>&1)
-      echo "emulator status=$bootanim"
-    done
+function waitForAVD {
+  local bootanim=""
+  export PATH=$(dirname $(dirname $(which android)))/platform-tools:$PATH
+  until [[ "$bootanim" =~ "stopped" ]]; do
+    sleep 5
+    bootanim=$(adb -e shell getprop init.svc.bootanim 2>&1)
+    echo "emulator status=$bootanim"
+  done
 }
