efda6da51e3268a4d285a4e79efcca082694e6ef
Romain Rochegude <RoRoche@users.noreply.github.com>
2017-02-16 21:30:44 +0100

    Update environmentSetup.sh

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -9,7 +9,7 @@ function getAndroidSDK {
   if [ ! -e $DEPS ]; then
     echo y | android update sdk -u -a -t android-21 &&
     echo y | android update sdk -u -a -t platform-tools &&
-    echo y | android update sdk -u -a -t build-tools-25.1.1 &&
+    echo y | android update sdk -u -a -t build-tools-23.0.3 &&
     echo y | android update sdk -u -a -t "extra-android-m2repository" &&
     echo y | android update sdk -u -a -t "extra-android-support" &&
     echo y | android update sdk -u -a -t "extra-google-m2repository" &&
